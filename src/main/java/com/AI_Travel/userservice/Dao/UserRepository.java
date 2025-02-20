af6fa7b41b8c8bf4349d4e1654caabefac7a9cbb
jeevanthecoder <jeevanharish897@gmail.com>
2025-02-20 22:33:05 +0530

    Changes made in UserRepository

--- src/main/java/com/AI_Travel/userservice/Dao/UserRepository.java
+++ src/main/java/com/AI_Travel/userservice/Dao/UserRepository.java
@@ -1,5 +1,6 @@
 package com.AI_Travel.userservice.Dao;
 import com.AI_Travel.userservice.Entities.Users;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,5 +11,7 @@ public interface UserRepository extends JpaRepository<Users,Integer> {
     public Optional<Users> findUserByUserName(final String userName);
 
     Optional<Users> findUserByUserEmail(final String userEmail);
+
+    Users findUsersByUserType(final String UserType);
 
 }
